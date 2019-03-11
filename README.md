2c2599d22eebfff789bb6796aabc4cf26426887d
barneyye(叶伟成) <barneyye@kugou.net>
2019-03-11 17:53:27 +0800

    add include & exclude metrics file

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ java -jar kudu-exporter-1.0-SNAPSHOT.jar --url http://localhost:8050/metrics
  param | describe | required
  ------------- | ------------- | -------------
  url | crawl kudu url data | true
- interval | crawl time interval, default 1000ms | false
  port | http port for prometheus, default 9098 | false
- exclude-file | TODO, for exclude unuseful metrics | false
+ interval | crawl time interval, default 1000ms | false
+ reload | reload inculde & exclude file, default 10000ms | false
+ exclude-file | for exclude unuseful metrics, one row one metric | false
+ include-file | for exclude unuseful metrics, one row one metric(highest priority) | false
--- src/main/java/org/yewc/prometheus/KuduExporter.java
+++ src/main/java/org/yewc/prometheus/KuduExporter.java
@@ -12,6 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.yewc.prometheus.util.HttpReqUtil;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.net.InetSocketAddress;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -34,39 +37,134 @@ public class KuduExporter {
     @Option(name="--url", usage="crape kudu url", required = true)
     private String url;
 
-    @Option(name="--interval", usage="crape kudu interval millis time")
-    private int interval = 1000;
-
     @Option(name="--port", usage="web port")
     private int port = 9098;
 
-    @Option(name="--exclude-file", usage="TODO: exclude metrics list, text file will be fine")
-    private String excludeFile;
+    @Option(name="--interval", usage="crape kudu interval millis time")
+    private int interval = 1000;
+
+    @Option(name="--reload", usage="reload inculde & exclude file interval millis time")
+    private int reload = 10000;
+
+    @Option(name="--exclude-file", usage="exclude metrics list, text file will be fine, one row one metric")
+    private String excludeFilePath;
+
+    @Option(name="--include-file", usage="include metrics list, text file will be fine, one row one metric, highest priority, only include metrics will be recode")
+    private String includeFilePath;
+
+    private final Set<String> excludeMetrics = new HashSet<>();
+    private final Set<String> includeMetrics = new HashSet<>();
+
+    private static volatile boolean PROCESS_LOCKED = true;
+    private static volatile boolean RELOAD_LOCKED = false;
+
+    private static File excludeFile;
+    private static File includeFile;
+
+    private static Long excludeFileModified;
+    private static Long includeFileModified;
 
     private void start() throws Exception {
         LOGGER.info("use url: " + url);
         LOGGER.info("use port: " + port);
         LOGGER.info("use interval: " + interval);
-        LOGGER.info("use exclude file: " + excludeFile);
+        LOGGER.info("use exclude file: " + excludeFilePath);
+        LOGGER.info("use include file: " + includeFilePath);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
+        if (includeFilePath != null && !"".equals(includeFilePath.trim())) {
+            includeFile = new File(includeFilePath);
+            includeFileModified = includeFile.lastModified();
+            readMetricsFile(includeFile, includeMetrics);
+            LOGGER.info("include metrics size: {}", includeMetrics.size());
+        }
+
+        if (includeMetrics.isEmpty() && excludeFilePath != null && !"".equals(excludeFilePath.trim())) {
+            excludeFile = new File(excludeFilePath);
+            excludeFileModified = excludeFile.lastModified();
+            readMetricsFile(excludeFile, excludeMetrics);
+            LOGGER.info("exclude metrics size: {}", excludeMetrics.size());
+        }
+
+        new Thread(() -> {
                 try {
                     while (true) {
                         Thread.sleep(interval);
+                        while (RELOAD_LOCKED) {
+                            Thread.sleep(10);
+                        }
+                        PROCESS_LOCKED = true;
                         processRequest();
+                        PROCESS_LOCKED = false;
+
+                        LOGGER.debug("metrics processed success!");
                     }
                 } catch (Exception e) {
                     LOGGER.error("", e);
                     System.exit(1);
                 }
-            }
         }).start();
+
+        if (includeFile != null || excludeFile != null) {
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        Thread.sleep(reload);
+                        if ((includeFile != null && includeFileModified != includeFile.lastModified())
+                                || (excludeFile != null && excludeFileModified != excludeFile.lastModified())) {
+                            if (includeFile != null) {
+                                includeFileModified = includeFile.lastModified();
+                            }
+
+                            if (excludeFile != null) {
+                                excludeFileModified = excludeFile.lastModified();
+                            }
+                        } else {
+                            continue;
+                        }
+
+                        while (PROCESS_LOCKED) {
+                            Thread.sleep(10);
+                        }
+                        RELOAD_LOCKED = true;
+
+                        if (includeFile != null) {
+                            includeMetrics.clear();
+                            readMetricsFile(includeFile, includeMetrics);
+                        } else if (excludeFile != null) {
+                            excludeMetrics.clear();
+                            readMetricsFile(excludeFile, excludeMetrics);
+                        }
+                        RELOAD_LOCKED = false;
+
+                        LOGGER.info("metrics file reloaded success!");
+                        LOGGER.info("include metrics size: {}", includeMetrics.size());
+                        LOGGER.info("exclude metrics size: {}", excludeMetrics.size());
+                    } catch (Exception e) {
+                        RELOAD_LOCKED = false;
+                        LOGGER.error("", e);
+                    }
+                }
+            }).start();
+        }
 
         InetSocketAddress socket = new InetSocketAddress(this.port);
 
         this.httpServer = new HTTPServer(socket, this.registry);
+    }
+
+    private void readMetricsFile(File file, Set<String> metricsSet) throws Exception {
+        if (!file.exists()) {
+            throw new RuntimeException("metrics file is not exists");
+        }
+
+        FileReader reader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String tempString = null;
+        while ((tempString = bufferedReader.readLine()) != null) {
+            metricsSet.add(tempString);
+        }
+        bufferedReader.close();
+        reader.close();
     }
 
     public void processRequest() throws Exception {
@@ -78,7 +176,7 @@ public class KuduExporter {
         JSONObject attributes;
         JSONArray metrics;
         Gauge tempGauge = null;
-        String id, type, key, name;
+        String id, type, key, name, orginName;
         String[] allKeys = null;
         Set<String> valueKeys = null;
         List<String> newLabelValues = null;
@@ -116,12 +214,29 @@ public class KuduExporter {
 
             for (int j = 0; j < metrics.size(); j++) {
                 metricData = metrics.getJSONObject(j);
-                name = PREFIX + metricData.getString("name");
-                if ((PREFIX + "state").equals(name)) {
+                orginName = metricData.getString("name");
+                if ("state".equals(orginName)) {
                     continue;
                 }
 
+                name = PREFIX + orginName;
                 key = name;
+
+                if (!includeMetrics.isEmpty()) {
+                    if (!includeMetrics.contains(orginName)) {
+                        if (gaugeMap.containsKey(key)) {
+                            registry.unregister(gaugeMap.get(key));
+                            gaugeMap.remove(key);
+                        }
+                        continue;
+                    }
+                } else if (!excludeMetrics.isEmpty() && excludeMetrics.contains(orginName)) {
+                    if (gaugeMap.containsKey(key)) {
+                        registry.unregister(gaugeMap.get(key));
+                        gaugeMap.remove(key);
+                    }
+                    continue;
+                }
 
                 try {
 
